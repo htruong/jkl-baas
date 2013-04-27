69e37b99d705400874472dcfcb2727104eb99cbf
Huan Truong <htruong@tnhh.net>
2013-04-27 09:52:24 -0500

    First version, works

--- main.go
+++ main.go
@@ -1,165 +1,224 @@
 package main
 
 import (
-	"github.com/howeyc/fsnotify"
-
+	"code.google.com/p/monnand-goconf"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/howeyc/fsnotify"
+	"io"
+	"io/ioutil"
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
+	"log"
+	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"strings"
-	"sync"
+	//"strings"
+	"time"
+	//"sync"
+)
+
+type SiteConf struct {
+	Name,
+	BaseURL,
+	HostName,
+	CloneURLType,
+	CloneURL,
+	NeedsDeployment,
+	APISecret string
+}
+
+type APIResponse struct {
+	Code    uint
+	Message string
+}
+
+var (
+	sitedir  = "sites"
+	gendir   = "_gen"
+	outdir   = "_out"
+	basedir  = ""
+	s3key    = ""
+	s3secret = ""
+	verbose  = true
 )
 
 var (
-	// directory where Jekyll will look to transform files
-	source = flag.String("source", "", "")
-
-	// directory where Jekyll will write files to
-	destination = flag.String("destination", "_site", "")
-
-	// fires up a server that will host your _site directory if True
-	server = flag.Bool("server", false, "")
-
-	// the port that the Jekyll server will run on
-	port = flag.String("server_port", ":4000", "")
-
-	// re-generates the site when files are modified.
-	auto = flag.Bool("auto", false, "")
-
-	// deploys the website to S3
-	deploy = flag.Bool("s3", false, "")
-
-	// serves the website from the specified base url
-	baseurl = flag.String("base-url", "", "")
-
-	// s3 access key
-	s3key = flag.String("s3_key", "", "")
-
-	// s3 secret key
-	s3secret = flag.String("s3_secret", "", "")
-
-	// s3 bucket name
-	s3bucket = flag.String("s3_bucket", "", "")
-
-	// runs Jekyll with verbose output if True
-	verbose = flag.Bool("verbose", false, "")
-
-	// displays the help / usage if True
-	help = flag.Bool("help", false, "")
+	// jekyll blog-as-a-service config files
+	globalconf = flag.String("conf", "jekyll-baas.conf", "Global config file")
+	sitesconf  = flag.String("sites", "sites.json", "Sites list file")
 )
 
-// Mutex used when doing auto-builds
-var mu sync.RWMutex
-
-func main() {
-
-	// Parse the input parameters
-	flag.BoolVar(help, "h", false, "")
-	flag.BoolVar(verbose, "v", false, "")
-	flag.Usage = usage
-	flag.Parse()
-
-	if *help {
-		flag.Usage()
-		os.Exit(0)
+func runWithTimeout(cmd *exec.Cmd) {
+	done := make(chan error)
+	if verbose {
+		stderr, _ := cmd.StderrPipe()
+		stdout, _ := cmd.StdoutPipe()
+		go io.Copy(os.Stderr, stderr)
+		go io.Copy(os.Stdout, stdout)
 	}
-
-	// User may specify the source as a non-flag variable
-	if flag.NArg() > 0 {
-		source = &flag.Args()[0]
-	}
-
-	// Convert the directory to an absolute path
-	src, _ := filepath.Abs(*source)
-	dest, _ := filepath.Abs(*destination)
-
-	// Change the working directory to the website's source directory
-	os.Chdir(src)
-
-	// Initialize the Jekyll website
-	site, err := NewSite(src, dest)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Set any site variables that were overriden / provided in the cli args
-	if *baseurl != "" || site.Conf.Get("baseurl") == nil { site.Conf.Set("baseurl", *baseurl) }
-
-	// Generate the static website
-	if err := site.Generate(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Deploys the static website to S3
-	if *deploy {
-
-		var conf *DeployConfig
-		// Read the S3 configuration details if not provided as
-		// command line
-		if *s3key == "" {
-			path := filepath.Join(site.Src, "_jekyll_s3.yml")
-			conf, err = ParseDeployConfig(path)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		} else {
-			// else use the command line args
-			conf = &DeployConfig{*s3key, *s3secret, *s3bucket}
+	cmd.Start()
+	go func() {
+		done <- cmd.Wait()
+	}()
+	select {
+	case <-time.After(30 * time.Second):
+		if err := cmd.Process.Kill(); err != nil {
+			log.Fatal("Failed to kill: ", err)
 		}
-
-		if err := site.Deploy(conf.Key, conf.Secret, conf.Bucket); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		<-done // allow goroutine to exit
+		log.Println("Process killed")
+	case err := <-done:
+		log.Printf("Process done with error = %v", err)
 	}
-
-	// If the auto option is enabled, use fsnotify to watch
-	// and re-generate the site if files change.
-	if *auto {
-		fmt.Printf("Listening for changes to %s\n", site.Src)
-		go watch(site)
-	}
-
-	// If the server option is enabled, launch a webserver
-	if *server {
-
-		// Change the working directory to the _site directory
-		//os.Chdir(dest)
-
-		// Create the handler to serve from the filesystem
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			mu.RLock()
-			defer mu.RUnlock()
-
-			base := site.Conf.GetString("baseurl")
-			path := r.URL.Path
-			pathList := filepath.SplitList(path)
-			if len(pathList) > 0 && pathList[0] == base {
-				path = strings.Join(pathList[len(pathList):], "/")
-			}
-
-			path = filepath.Clean(path)
-			path = filepath.Join(dest, path)
-			http.ServeFile(w, r, path)
-		})
-
-		// Serve the website from the _site directory
-		fmt.Printf("Starting server on port %s\n", *port)
-		if err := http.ListenAndServe(*port, nil); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	}
-
-	os.Exit(0)
 }
 
-func watch(site *Site) {
+func jekyllProcessorConsumer(ch chan SiteConf) {
+	log.Printf("Site renderer module launching...\n")
+	for {
+		log.Printf("Waiting for new changes to process...\n")
+
+		job := <-ch
+		log.Printf("--- Got job --> %s ---", job.Name)
+
+		log.Printf("Pulling from source...")
+		// Convert the directory to an absolute path
+
+		src, _ := filepath.Abs(filepath.Join(basedir, sitedir, job.HostName))
+		dest, _ := filepath.Abs(filepath.Join(basedir, gendir, job.HostName))
+		outd, _ := filepath.Abs(filepath.Join(basedir, outdir, job.HostName))
+
+		gitpullcmd := exec.Command("git", "--git-dir="+src+"/.git", "--work-tree="+src, "pull")
+
+		runWithTimeout(gitpullcmd)
+
+		log.Printf(" Done!\n")
+		log.Printf("Generating static site...\n")
+
+		// Change the working directory to the website's source directory
+		os.Chdir(src)
+
+		// Initialize the Jekyll website
+		site, err := NewSite(src, dest)
+		if err != nil {
+			fmt.Printf("Error on site %s while trying to initialize: %v. This site will be temporary disabled!\n", job.Name, err)
+			//os.Exit(1)
+			return
+		}
+
+		if len(job.BaseURL) != 0 {
+			site.Conf.Set("baseurl", job.BaseURL)
+		}
+
+		// Generate the static website
+		if err := site.Generate(); err != nil {
+			fmt.Printf("Error on site %s while trying to generate static content: %v\n", job.Name, err)
+			//os.Exit(1)
+			return
+		}
+
+		log.Printf(" Done!\n")
+
+		log.Printf("Calculating differences...\n")
+		// Now sync it to the outdir
+		log.Printf("The gen dir is %s out dir is %s", dest, outd)
+
+		if err := os.MkdirAll(outd, 0755); err != nil {
+			log.Printf("Makedir %s error?\n", outd)
+			continue
+		}
+		// The ending slash makes rsync sync the same level directory
+		rsynccmd := exec.Command("rsync", "--delete", "--size-only", "--recursive", dest+"/", outd)
+
+		runWithTimeout(rsynccmd)
+
+		log.Printf(" Done!\n")
+	}
+}
+
+func watch(job SiteConf, ch chan SiteConf) {
+
+	// Deploys the static website to S3
+	var conf *DeployConfig
+	// Read the S3 configuration details if there is a s3 conf file
+
+	path := filepath.Join(basedir, sitedir, job.HostName, "_jekyll_s3.yml")
+	fi, err := os.Stat(path)
+	if fi != nil && err == nil {
+		conf, err = ParseDeployConfig(path)
+		if err != nil {
+			fmt.Printf("Error on site %s while trying to parse deployment config: %v\n", job.Name, err)
+			//os.Exit(1)
+			return
+		}
+	} else {
+		// else use the command line args
+		conf = &DeployConfig{s3key, s3secret, job.HostName}
+	}
+
+	/*
+		if err := site.Deploy(conf.Key, conf.Secret, conf.Bucket); err != nil {
+			fmt.Printf("Error on site %s while trying to deploy to s3: %v\n", job.Name, err)
+			//os.Exit(1)
+			return
+		}
+	*/
+
+	// Get recursive list of directories to watch
+	src, _ := filepath.Abs(filepath.Join(basedir, outdir, job.HostName))
+
+	uploaderqueue := make(chan string)
+
+	go func() {
+		auth := aws.Auth{conf.Key, conf.Secret}
+		b := s3.New(auth, aws.USEast).Bucket(conf.Bucket)
+		lastAuth := time.Now()
+		for {
+			fn := <-uploaderqueue
+			fi, err := os.Stat(fn)
+
+			if err != nil {
+				log.Printf("There was an error getting the file %s: %v", fn, err)
+				continue
+			}
+
+			// if it's too long ago, then reauthenticate
+			if time.Since(lastAuth).Minutes() > 2 {
+				auth = aws.Auth{conf.Key, conf.Secret}
+				b = s3.New(auth, aws.USEast).Bucket(conf.Bucket)
+			}
+
+			if fi.IsDir() {
+				continue
+			}
+
+			rel, _ := filepath.Rel(src, fn)
+			typ := mime.TypeByExtension(filepath.Ext(rel))
+			content, err := ioutil.ReadFile(fn)
+			log.Printf("[%s] Uploading %s...", job.HostName, rel)
+			if err != nil {
+				continue
+			}
+
+			// try to upload the file ... sometimes this fails due to amazon
+			// issues. If so, we'll re-try
+			if err := b.Put(rel, content, typ, s3.PublicRead); err != nil {
+				time.Sleep(100 * time.Millisecond) // sleep so that we don't immediately retry
+				err2 := b.Put(rel, content, typ, s3.PublicRead)
+				if err2 != nil {
+					log.Printf(" Failed!\n")
+					continue
+				}
+			}
+			log.Printf(" Success!")
+
+			lastAuth = time.Now()
+
+		}
+	}()
 
 	// Setup the inotify watcher
 	watcher, err := fsnotify.NewWatcher()
@@ -168,8 +227,8 @@ func watch(site *Site) {
 		return
 	}
 
-	// Get recursive list of directories to watch
-	for _, path := range dirs(site.Src) {
+	log.Printf("[%s] Watching %s\n", job.HostName, src)
+	for _, path := range dirs(src) {
 		if err := watcher.Watch(path); err != nil {
 			fmt.Println(err)
 			return
@@ -180,50 +239,126 @@ func watch(site *Site) {
 		select {
 		case ev := <-watcher.Event:
 			// Ignore changes to the _site directoy, hidden, or temp files
-			if !strings.HasPrefix(ev.Name, site.Dest) && !isHiddenOrTemp(ev.Name) {
-				fmt.Println("Event:", ev.Name)
-				recompile(site)
+			if !isHiddenOrTemp(ev.Name) && (ev.IsCreate() || ev.IsModify()) {
+				log.Printf("[%s] File created or modified: %s", job.HostName, ev.Name)
+				uploaderqueue <- ev.Name
+				fi, _ := os.Stat(ev.Name)
+				if fi.IsDir() {
+					watcher.Watch(ev.Name)
+				}
 			}
+
 		case err := <-watcher.Error:
 			fmt.Println("inotify error:", err)
 		}
 	}
 }
 
-func recompile(site *Site) {
-	mu.Lock()
-	defer mu.Unlock()
-	site.Reload()
-	site.Generate()
-}
+func main() {
+	flag.Parse()
 
-func logf(msg string, args ...interface{}) {
-	if *verbose {
-		println(fmt.Sprintf(msg, args...))
+	c, err := conf.ReadConfigFile(*globalconf)
+
+	if err != nil {
+		log.Fatal("Error while opening global config file: %s. Bailing out!\n", err)
 	}
-}
 
-var usage = func() {
-	fmt.Println(`Usage: jkl [OPTION]... [SOURCE]
+	port, err := c.GetString("general", "port")
+	if err != nil {
+		port = "9999"
+	}
 
-      --auto           re-generates the site when files are modified
-      --base-url       serve website from a given base URL
-      --source         changes the dir where Jekyll will look to transform files
-      --destination    changes the dir where Jekyll will write files to
-      --server         starts a server that will host your _site directory
-      --server-port    changes the port that the Jekyll server will run on
-      --s3             copies the _site directory to s3
-      --s3_key         aws access key use for s3 authentication
-      --s3_secret      aws secret key use for s3 authentication
-      --s3_bucket      name of the s3 bucket
-  -v, --verbose        runs Jekyll with verbose output
-  -h, --help           display this help and exit
+	// base dir
+	basedir, err = c.GetString("general", "base_dir")
+	if err != nil {
+		basedir = ""
+	}
 
-Examples:
-  jkl                 generates site from current working directory
-  jkl --server        generates site and serves at localhost:4000
-  jkl /path/to/site   generates site from source dir /path/to/site
+	// s3 access key
+	s3key, err = c.GetString("s3", "key")
+	if err != nil {
+		log.Fatal("No default global S3 key found. Configure yours in the global config file!\n", err)
+	}
 
-Report bugs to <https://github.com/bradrydzewski/jkl/issues>
-Jekyll home page: <https://github.com/bradrydzewski/jkl>`)
+	// s3 secret key
+	s3secret, err = c.GetString("s3", "secret")
+
+	if err != nil {
+		log.Fatal("No default global S3 secret found. Configure yours in the global config file!\n", err)
+	}
+
+	fi, err := os.Open(*sitesconf)
+
+	work := make(chan SiteConf)
+
+	go jekyllProcessorConsumer(work)
+
+	if err != nil {
+		fmt.Printf("File error while trying to open the sites config: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Printf("Reading all sites configuration... Please wait!\n")
+
+	dec := json.NewDecoder(fi)
+
+	allSites := []SiteConf{}
+
+	for {
+		var s SiteConf
+		if err := dec.Decode(&s); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("[conf] Got site: %s: %s!\n", s.Name, s.HostName)
+		allSites = append(allSites, s)
+	}
+
+	for _, s := range allSites {
+		log.Printf("Site: %s [%s]\n", s.Name, s.HostName)
+		go watch(s, work)
+		work <- s
+	}
+
+	fi.Close()
+
+	// Create the handler to serve from the filesystem
+	http.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		hostname := r.URL.Query().Get("hostname")
+		foundhost := false
+		for _, s := range allSites {
+			if s.HostName == hostname {
+				work <- s
+				foundhost = true
+				break
+			}
+		}
+
+		msg := APIResponse{
+			Code:    400,
+			Message: "Command executed successfully",
+		}
+
+		if !foundhost {
+			msg = APIResponse{
+				Code:    404,
+				Message: "Host not found",
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/javascript")
+		w.Header().Set("Cache-Control", "no-cache")
+		msgToSend, _ := json.Marshal(msg)
+
+		w.Write(msgToSend)
+	})
+
+	fmt.Printf("Starting server on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
--- site.go
+++ site.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"launchpad.net/goamz/aws"
 	"launchpad.net/goamz/s3"
+	"log"
 	"mime"
 	"os"
 	"path/filepath"
@@ -20,13 +21,13 @@ var (
 )
 
 type Site struct {
-	Src    string  // Directory where Jekyll will look to transform files
-	Dest   string  // Directory where Jekyll will write files to
-	Conf   Config  // Configuration date from the _config.yml file
+	Src  string // Directory where Jekyll will look to transform files
+	Dest string // Directory where Jekyll will write files to
+	Conf Config // Configuration date from the _config.yml file
 
-	posts []Page   // Posts thet need to be generated
-	pages []Page   // Pages that need to be generated
-	files []string // Static files to get copied to the destination
+	posts []Page             // Posts thet need to be generated
+	pages []Page             // Pages that need to be generated
+	files []string           // Static files to get copied to the destination
 	templ *template.Template // Compiled templates
 }
 
@@ -35,15 +36,15 @@ func NewSite(src, dest string) (*Site, error) {
 	// Parse the _config.yml file
 	path := filepath.Join(src, "_config.yml")
 	conf, err := ParseConfig(path)
-	logf(MsgUsingConfig, path)
+	log.Printf(MsgUsingConfig, path)
 	if err != nil {
 		return nil, err
 	}
 
-	site := Site {
-		Src  : src,
-		Dest : dest,
-		Conf : conf,
+	site := Site{
+		Src:  src,
+		Dest: dest,
+		Conf: conf,
 	}
 
 	// Recursively process all files in the source directory
@@ -79,12 +80,20 @@ func (s *Site) Generate() error {
 
 	// Remove previously generated site, and then (re)create the
 	// destination directory
-	if err := s.Clear(); err != nil { return err }
-	if err := s.Prep() ; err != nil { return err }
+	if err := s.Clear(); err != nil {
+		return err
+	}
+	if err := s.Prep(); err != nil {
+		return err
+	}
 
 	// Generate all Pages and Posts and static files
-	if err := s.writePages() ; err != nil { return err }
-	if err := s.writeStatic(); err != nil { return err }
+	if err := s.writePages(); err != nil {
+		return err
+	}
+	if err := s.writeStatic(); err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -104,15 +113,15 @@ func (s *Site) Deploy(user, pass, url string) error {
 		rel, _ := filepath.Rel(s.Dest, fn)
 		typ := mime.TypeByExtension(filepath.Ext(rel))
 		content, err := ioutil.ReadFile(fn)
-		logf(MsgUploadFile, rel)
+		log.Printf(MsgUploadFile, rel)
 		if err != nil {
 			return err
 		}
-		
+
 		// try to upload the file ... sometimes this fails due to amazon
 		// issues. If so, we'll re-try
 		if err := b.Put(rel, content, typ, s3.PublicRead); err != nil {
-			time.Sleep(100*time.Millisecond) // sleep so that we don't immediately retry
+			time.Sleep(100 * time.Millisecond) // sleep so that we don't immediately retry
 			return b.Put(rel, content, typ, s3.PublicRead)
 		}
 
@@ -136,37 +145,43 @@ func (s *Site) read() error {
 
 		rel, _ := filepath.Rel(s.Src, fn)
 		switch {
-		case err != nil :
+		case err != nil:
 			return nil
 
 		// Ignore directories
-		case fi.IsDir() :
+		case fi.IsDir():
 			return nil
 
 		// Ignore Hidden or Temp files
 		// (starting with . or ending with ~)
-		case isHiddenOrTemp(rel) :
+		case isHiddenOrTemp(rel):
 			return nil
 
 		// Parse Templates
-		case isTemplate(rel) :
+		case isTemplate(rel):
 			layouts = append(layouts, fn)
 
 		// Parse Posts
-		case isPost(rel) :
+		case isPost(rel):
 			post, err := ParsePost(rel)
-			if err != nil { return err }
+			if err != nil {
+				return err
+			}
 			// TODO: this is a hack to get the posts in rev chronological order
-			s.posts = append([]Page{post}, s.posts...)//s.posts, post)
+			s.posts = append([]Page{post}, s.posts...) //s.posts, post)
 
 		// Parse Pages
-		case isPage(rel) :
+		case isPage(rel):
 			page, err := ParsePage(rel)
-			if err != nil { return err }
-			s.pages = append(s.pages, page)
+			if err != nil {
+				return err
+			}
+			if page.GetTitle() != "" {
+				s.pages = append(s.pages, page)
+			}
 
 		// Move static files, no processing required
-		case isStatic(rel) :
+		case isStatic(rel):
 			s.files = append(s.files, rel)
 		}
 		return nil
@@ -188,6 +203,7 @@ func (s *Site) read() error {
 
 	// Add the posts, timestamp, etc to the Site Params
 	s.Conf.Set("posts", s.posts)
+	s.Conf.Set("pages", s.pages)
 	s.Conf.Set("time", time.Now())
 	s.calculateTags()
 	s.calculateCategories()
@@ -209,7 +225,6 @@ func (s *Site) writePages() error {
 	for _, page := range pages {
 		url := page.GetUrl()
 		layout := page.GetLayout()
-		
 
 		// is the layout provided? or is it nil /empty?
 		//layoutNil := layout == "" || layout == "nil"
@@ -231,7 +246,7 @@ func (s *Site) writePages() error {
 		//}
 
 		//data passed in to each template
-		data := map[string]interface{} {
+		data := map[string]interface{}{
 			"site": s.Conf,
 			"page": page,
 		}
@@ -242,18 +257,20 @@ func (s *Site) writePages() error {
 			// this code will add the page to the list of templates,
 			// will execute the template, and then set the content
 			// to the rendered template
-			t, err := s.templ.New(url).Parse(content);
-			if err != nil { return err }
+			t, err := s.templ.New(url).Parse(content)
+			if err != nil {
+				return err
+			}
 			var buf bytes.Buffer
-			err = t.ExecuteTemplate(&buf, url, data);
-			if err != nil { return err }
+			err = t.ExecuteTemplate(&buf, url, data)
+			if err != nil {
+				return err
+			}
 			content = buf.String()
 		}
 
 		// add document body to the map
 		data["content"] = content
-
-
 
 		// write the template to a buffer
 		// NOTE: if template is nil or empty, then we should parse the
@@ -271,13 +288,13 @@ func (s *Site) writePages() error {
 			s.templ.ExecuteTemplate(&buf, layout, data)
 		}
 
-		logf(MsgGenerateFile, url)
+		log.Printf(MsgGenerateFile, url)
 		if err := ioutil.WriteFile(f, buf.Bytes(), 0644); err != nil {
 			return err
 		}
 	}
 
-	return nil	
+	return nil
 }
 
 // Helper function to write all static files to the destination directory
@@ -287,8 +304,8 @@ func (s *Site) writeStatic() error {
 
 	for _, file := range s.files {
 		from := filepath.Join(s.Src, file)
-		to   := filepath.Join(s.Dest, file)
-		logf(MsgCopyingFile, file)
+		to := filepath.Join(s.Dest, file)
+		log.Printf(MsgCopyingFile, file)
 		if err := copyTo(from, to); err != nil {
 			return err
 		}
@@ -307,7 +324,7 @@ func (s *Site) calculateCategories() {
 			if posts, ok := categories[category]; ok == false {
 				categories[category] = append(posts, post)
 			} else {
-				categories[category] = []Page{ post }
+				categories[category] = []Page{post}
 			}
 		}
 	}
@@ -325,7 +342,7 @@ func (s *Site) calculateTags() {
 			if posts, ok := tags[tag]; ok == false {
 				tags[tag] = append(posts, post)
 			} else {
-				tags[tag] = []Page{ post }
+				tags[tag] = []Page{post}
 			}
 		}
 	}
